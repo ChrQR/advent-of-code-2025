7554b392319be65bdf965bc6a947300b5a942c8b
Christian Rannes <christian@rannes.dev>
2025-12-05 21:59:25 +0100

    solved 3-2

--- 03/02/main.go
+++ 03/02/main.go
@@ -1,8 +1,55 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
-	// file, _ := os.Open("./input.txt")
-	//
-	//
-	// fmt.Print(total)
+	file, _ := os.Open("./input.txt")
+
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+
+	for scanner.Scan() {
+		total += highestNumber(scanner.Text())
+	}
+	fmt.Print(total)
+}
+
+func highestNumber(input string) int {
+	rslt := ""
+	highest := 0
+	highestIdx := 0
+	i := 0
+	for {
+		num, _ := strconv.Atoi(string(input[i]))
+
+		if num > highest {
+			highest = num
+			highestIdx = i
+		}
+		// len 24
+		// i 12
+
+		if i+(12-len(rslt)) == len(input) {
+			rslt += string(input[highestIdx])
+			i = highestIdx
+			highest = 0
+		}
+
+		if len(rslt) == 12 {
+			break
+		}
+
+		i++
+
+	}
+	rsltInt, _ := strconv.Atoi(rslt)
+	fmt.Printf("Result: %d\n", rsltInt)
+	fmt.Printf("Result len: %d \n", len(rslt))
+	return rsltInt
 }
--- 03/02/main_test.go
+++ 03/02/main_test.go
@@ -1,35 +1,27 @@
 package main
 
-//
-// import (
-// 	"bufio"
-// 	"strings"
-// 	"testing"
-// )
-//
-// func TestMaxJolt(t *testing.T) {
-// 	cases := []struct {
-// 		Input    []string
-// 		Expected int
-// 	}{
-// 		{
-// 			Input: []string{
-// 				"3233434223",
-// 				"3253614853",
-// 				"9227239868",
-// 			},
-// 			Expected: 44 + 85 + 99,
-// 		},
-// 	}
-//
-// 	for _, c := range cases {
-// 		t.Run("test TestMaxJolt", func(t *testing.T) {
-// 			input := strings.Join(c.Input, "\n")
-// 			scanner := bufio.NewScanner(strings.NewReader(input))
-// 			total := MaxJolt(scanner)
-// 			if total != c.Expected {
-// 				t.Errorf("expected: %d, got %d", c.Expected, total)
-// 			}
-// 		})
-// 	}
-// }
+import (
+	"testing"
+)
+
+func TestMaxJolt(t *testing.T) {
+	cases := []struct {
+		Input    string
+		Expected int
+	}{
+		{
+			Input:    "3233434223352253322244323562413222322422522622312422332123223123235422212196323222332232332242222211",
+			Expected: 963342222211,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("test TestMaxJolt", func(t *testing.T) {
+			num := highestNumber(c.Input)
+			if num != c.Expected {
+				t.Log(c.Expected > num)
+				t.Errorf("expected: %d, got %d", c.Expected, num)
+			}
+		})
+	}
+}
